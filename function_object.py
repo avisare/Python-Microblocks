3adc365d3ba206b0fcf829293f267cb233d4def8
noam2003 <noammiz918@gmail.com>
2021-03-08 17:29:38 +0200

    change the type of the time to float

--- function_object.py
+++ function_object.py
@@ -1,7 +1,7 @@
 
 
 class FunctionObject:
-    def __init__(self, function_ptr: 'function' = None, function_args: 'tuple' = (), timeout_seconds = None):
+    def __init__(self, function_ptr: 'function' = None, function_args: 'tuple' = (), timeout_seconds: 'float' = None):
         self.function_ptr = function_ptr
         self.function_args = function_args
         self.timeout_seconds = timeout_seconds
