c630eefee8487d3c66eb179e197f7e7dff9e4f21
noam2003 <noammiz918@gmail.com>
2021-03-09 09:51:27 +0200

    add the pypsexec working

--- pypsexec_client.py
+++ pypsexec_client.py
@@ -1,21 +1,23 @@
 from pypsexec.client import Client
 # creates an encrypted connection to the host with the username and password
+# 192.168.2.16  RFManager
 c = Client("127.0.0.1", username="Administrator", password="1234")
 c.connect()
 try:
+
     c.create_service()
 
     # After creating the service, you can run multiple exe's without
     # reconnecting
 
     # run a simple cmd.exe program with arguments
-    stdout, stderr, rc = c.run_executable("cmd.exe",
-                                          arguments="/c echo Hello World")
-    print(stdout, rc)
+    # stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c echo hello world")
+    stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c cd.. & cd.. & cd test_remote & py start_server.py")
+    print(stdout, stderr)
 
     # run whoami.exe as the SYSTEM account
     stdout, stderr, rc = c.run_executable("whoami.exe", use_system_account=True)
-    print(stdout, rc)
+    print(stdout)
 finally:
     c.remove_service()
     c.disconnect()
