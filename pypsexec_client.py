dd27b69a6075fa4d05cc500a4e24cc66369be160
noam2003 <noammiz918@gmail.com>
2021-03-09 11:40:58 +0200

    the remote local works !

--- pypsexec_client.py
+++ pypsexec_client.py
@@ -1,7 +1,7 @@
 from pypsexec.client import Client
 # creates an encrypted connection to the host with the username and password
-# 192.168.2.16  RFManager
-c = Client("127.0.0.1", username="Administrator", password="1234")
+# 127.0.0.1
+c = Client("192.168.2.16", username="RFManager", password="1234")
 c.connect()
 try:
 
@@ -10,9 +10,8 @@ try:
     # After creating the service, you can run multiple exe's without
     # reconnecting
 
-    # run a simple cmd.exe program with arguments
-    # stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c echo hello world")
-    stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c cd.. & cd.. & cd test_remote & py start_server.py")
+    stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c echo hello world")
+    #stdout, stderr, rc = c.run_executable("cmd.exe", arguments="/c cd.. & cd.. & cd test_remote & py start_server.py")
     print(stdout, stderr)
 
 finally:
